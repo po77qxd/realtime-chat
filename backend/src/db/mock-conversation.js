467200e5b9071fae73230a2d2c5d17f2befaf064
Bastien <po77qxd@eduvaud.ch>
2026-01-12 14:18:31 +0100

    feat(db). ajout des mocks

---
+++ backend/src/db/mock-conversation.js
@@ -0,0 +1,16 @@
+let conversations = [
+  {
+    name: "General",
+    admin_user_id: 1,
+  },
+  {
+    name: "Tech",
+    admin_user_id: 1,
+  },
+  {
+    name: "Random",
+    admin_user_id: 2,
+  },
+];
+
+export { conversations };
---
+++ backend/src/db/mock-user-conversation.js
@@ -0,0 +1,13 @@
+let userConversations = [
+  { user_id: 1, conversation_id: 1 },
+  { user_id: 2, conversation_id: 1 },
+  { user_id: 3, conversation_id: 1 },
+
+  { user_id: 1, conversation_id: 2 },
+  { user_id: 2, conversation_id: 2 },
+
+  { user_id: 2, conversation_id: 3 },
+  { user_id: 3, conversation_id: 3 },
+];
+
+export { userConversations }
