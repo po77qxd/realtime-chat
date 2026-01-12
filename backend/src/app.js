4f88cdda2438cf44a61a73136048b622c031a42f
Bastien <po77qxd@eduvaud.ch>
2026-01-12 14:19:28 +0100

    feat(app): faire que le backend run

---
+++ backend/src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import { sequelize, initDb } from "./db/sequelize.js";
+
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({mesage: "hello"})
+})
+
+app.use(({ res }) => {
+  const message =
+    "Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.";
+  res.status(404).json(message);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
+
+
+sequelize
+  .authenticate()
+  .then((_) => console.log("Connexion à la base de données réussie."))
+  .catch((error) => console.error("Erreur de connexion à la base de données"));
+
+initDb();
