398853dfd3825e4eb09f1c247e8a1ec6cd579989
Bastien Segalen <po77qxd@eduvaud.ch>
2026-01-14 10:14:38 +0100

    fix(auth): fix le probleme de cookie

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { sequelize, initDb } from "./db/sequelize.js";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
 
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.json({mesage: "hello"})
