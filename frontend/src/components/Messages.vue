1856f47e6f212e49a28011565ebf0c60df32274a
Bastien <po77qxd@eduvaud.ch>
2026-01-28 16:04:26 +0100

    refactor(frontend): transformer les messages en composant

---
+++ frontend/src/components/Messages.vue
@@ -0,0 +1,225 @@
+<script setup>
+import { ref, onMounted, nextTick, watch, computed } from 'vue'
+import { useUserStore } from '@/stores/user'
+import DOMPurify from 'dompurify'
+import { marked } from 'marked'
+import conversationService from '@/services/conversationService'
+
+const props = defineProps(['messages', 'shownConvId', 'shownConvAdminId'])
+
+const userStore = useUserStore()
+
+const messages = computed(() => props.messages)
+const shownConvId = computed(() => props.shownConvId)
+const shownConvAdminId = computed(() => props.shownConvAdminId)
+
+const messageMenuShown = ref(null) //id of the message where the menu is shown
+const editInputShown = ref(null) //id of the message being modified
+const messagetoEdit = ref(null)
+const editMessageTextarea = ref(null)
+
+function showMessageMenu(id) {
+	messageMenuShown.value = id
+}
+
+function closeMessageMenu() {
+	messageMenuShown.value = null
+	//close the edit menu too
+	editInputShown.value = null
+	messagetoEdit.value = ''
+}
+
+async function showEditMessage(message) {
+	editInputShown.value = message.message_id
+	messagetoEdit.value = message.text
+}
+
+function closeEditMessage() {
+	editInputShown.value = null
+	messagetoEdit.value = ''
+}
+
+async function editMessage(message_id) {
+	if (!messagetoEdit.value.trim()) return
+
+	conversationService
+		.editMessage(message_id, shownConvId.value, messagetoEdit.value)
+		.then((response) => {
+			closeEditMessage()
+			//console.log(response.data.data)
+		})
+		.catch((error) => {
+			console.log(error)
+		})
+}
+
+async function deleteMessage(messageId) {
+	conversationService
+		.deletedMessage(messageId, shownConvId.value)
+		.then((response) => {
+			// console.log(response.data)
+		})
+		.catch((error) => {
+			console.log(error)
+		})
+}
+
+function shouldShowUsername(index) {
+	if (index == 0) return true
+	//si le message precedant a été envoyé par un user différent, on affiche le username, sinon non
+	return messages.value[index].user_id != messages.value[index - 1].user_id
+}
+
+function parseMarkdown(text) {
+	return DOMPurify.sanitize(marked.parse(text))
+}
+
+watch(messagetoEdit, async () => {
+	await nextTick()
+
+	const textarea = editMessageTextarea.value[0]
+	if (!textarea) return
+
+	//reset la hauteur
+	textarea.style.height = 'auto'
+
+	const style = window.getComputedStyle(textarea)
+	const lineHeight = parseFloat(style.lineHeight)
+	const padding = parseFloat(style.paddingTop) + parseFloat(style.paddingBottom)
+
+	const maxLines = 5
+	const maxHeight = lineHeight * maxLines + padding
+
+	textarea.style.height = Math.min(textarea.scrollHeight, maxHeight) + 'px'
+	textarea.style.height = textarea.scrollHeight + 'px'
+	textarea.style.width = '50%'
+})
+</script>
+
+<template>
+	<div class="messages">
+		<div
+			v-for="(message, index) in messages"
+			class="message"
+			:class="{ 'self-message': message.user_id == userStore.user.user_id }"
+		>
+			<div class="message-sender" v-if="shouldShowUsername(index)">
+				{{ message.User.name }}
+			</div>
+			<div
+				class="message-text"
+				v-if="!editInputShown || editInputShown != message.message_id"
+				@click="showMessageMenu(message.message_id)"
+				v-html="parseMarkdown(message.text)"
+			></div>
+			<form class="edit-message" v-else @submit.prevent="editMessage(message.message_id)">
+				<textarea
+					placeholder="Modifier le message"
+					v-model="messagetoEdit"
+					ref="editMessageTextarea"
+				></textarea>
+				<button @click="closeEditMessage" type="button">
+					<img src="../assets/close.png" />
+				</button>
+				<button type="submit"><img src="../assets/check.png" /></button>
+			</form>
+			<div
+				class="message-menu"
+				v-if="
+					messageMenuShown &&
+					messageMenuShown == message.message_id &&
+					(message.user_id == userStore.user.user_id || //afficher le menu seulement sur nos message ou sur tout les message si on est l'admin de la conversation actuelle
+						userStore.user.user_id == shownConvAdminId)
+				"
+			>
+				<button @click="showEditMessage(message)">
+					<img src="../assets/edit.png" />
+				</button>
+				<button @click="deleteMessage(message.message_id)">
+					<img src="../assets/delete.png" />
+				</button>
+				<button @click.stop="closeMessageMenu">
+					<img src="../assets/close.png" />
+				</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style scoped>
+.messages {
+	flex: 1;
+	overflow-y: auto;
+}
+
+.message {
+	display: flex;
+	flex-direction: column;
+	gap: 4px;
+	margin-top: 4px;
+}
+
+.message-sender {
+	font-size: 14px;
+	font-weight: bold;
+	margin-top: 5px;
+}
+
+.message-text {
+	background: blue;
+	padding: 0px 10px;
+	width: fit-content;
+	max-width: 50%;
+
+	word-wrap: break-word;
+	overflow-wrap: break-word;
+	overflow-wrap: anywhere;
+	text-align: left;
+	white-space: normal;
+}
+
+.message-text :link {
+	color: #00ffff;
+}
+
+/* deep pour que le css s'applique*/
+:deep(.message-text img) {
+	max-width: 100%;
+}
+
+.message-menu button {
+	width: 40px;
+	margin: 5px;
+}
+
+.message-menu button img {
+	width: 100%;
+}
+
+.edit-message textarea {
+	padding: 10px;
+	margin: 5px;
+	line-height: 20px;
+	resize: none;
+}
+
+.edit-message button {
+	width: 40px;
+	margin: 5px;
+}
+
+.edit-message button img {
+	width: 100%;
+}
+
+.self-message {
+	align-self: flex-end;
+	text-align: right;
+	margin-right: 15px;
+}
+
+.self-message .message-text {
+	background: darkblue;
+	margin-left: auto;
+}
+</style>
--- frontend/src/views/UserHomeView.vue
+++ frontend/src/views/UserHomeView.vue
@@ -5,11 +5,10 @@ import userService from '@/services/userService'
 
 import userConversationService from '@/services/userConversationService'
 import { useUserStore } from '@/stores/user'
-import DOMPurify from 'dompurify'
-import { marked } from 'marked'
 
 import { socket } from '@/main'
 import MessageBar from '@/components/MessageBar.vue'
+import Messages from '@/components/Messages.vue'
 
 const userStore = useUserStore()
 
@@ -19,9 +18,6 @@ const shownConvId = ref(null)
 const shownConvAdminId = ref(null)
 const showCreateConv = ref(false)
 const convName = ref('')
-const messageMenuShown = ref(null) //id of the message where the menu is shown
-const editInputShown = ref(null) //id of the message being modified
-const messagetoEdit = ref(null)
 const joinConvShown = ref(false)
 const convToJoinList = ref(null)
 const searchConvValue = ref(null)
@@ -34,8 +30,6 @@ const userListShown = ref(false)
 const messagesContainer = ref(null)
 const isUserAtBottom = ref(true)
 const SCROLL_THRESHOLD = 50 //px
-
-const editMessageTextarea = ref(null)
 
 //sockets:
 socket.on('new_message', async (message) => {
@@ -85,7 +79,7 @@ socket.on('user_join_conv', (user) => {
 
 socket.on('user_online', (userId) => {
 	if (!userListShown) return
-	const userOnlineIndex = userList.value.findIndex((user) => user.user_id == userId)
+	const userOnlineIndex = userList.value?.findIndex((user) => user.user_id == userId)
 	userList.value[userOnlineIndex].isOnline = true
 })
 socket.on('user_offline', (userId) => {
@@ -222,52 +216,6 @@ async function createConv() {
 		})
 }
 
-function showMessageMenu(id) {
-	messageMenuShown.value = id
-}
-
-function closeMessageMenu() {
-	messageMenuShown.value = null
-	//close the edit menu too
-	editInputShown.value = null
-	messagetoEdit.value = ''
-}
-
-async function showEditMessage(message) {
-	editInputShown.value = message.message_id
-	messagetoEdit.value = message.text
-}
-
-function closeEditMessage() {
-	editInputShown.value = null
-	messagetoEdit.value = ''
-}
-
-async function editMessage(message_id) {
-	if (!messagetoEdit.value.trim()) return
-
-	conversationService
-		.editMessage(message_id, shownConvId.value, messagetoEdit.value)
-		.then((response) => {
-			closeEditMessage()
-			//console.log(response.data.data)
-		})
-		.catch((error) => {
-			console.log(error)
-		})
-}
-
-async function deleteMessage(messageId) {
-	conversationService
-		.deletedMessage(messageId, shownConvId.value)
-		.then((response) => {
-			// console.log(response.data)
-		})
-		.catch((error) => {
-			console.log(error)
-		})
-}
-
 function showJoinConv() {
 	if (joinConvShown.value) {
 		//montrer la page mes conversations
@@ -363,7 +311,7 @@ async function deleteConv(convId) {
 }
 
 function onScroll() {
-	const mc = messagesContainer.value
+	const mc = messagesContainer.value.$el
 	if (!mc) return
 
 	const distanceFromBottom = mc.scrollHeight - mc.scrollTop - mc.clientHeight
@@ -372,47 +320,16 @@ function onScroll() {
 }
 
 function scrollToBottom() {
-	const mc = messagesContainer.value
+	const mc = messagesContainer.value.$el
 	if (!mc) return
 
 	mc.scrollTop = mc.scrollHeight
-}
-
-function shouldShowUsername(index) {
-	if (index == 0) return true
-	//si le message precedant a été envoyé par un user différent, on affiche le username, sinon non
-	return messages.value[index].user_id != messages.value[index - 1].user_id
 }
 
 //hide or show userlist
 function handleUserListState() {
 	userListShown.value = !userListShown.value
 }
-
-function parseMarkdown(text) {
-	return DOMPurify.sanitize(marked.parse(text))
-}
-
-watch(messagetoEdit, async () => {
-	await nextTick()
-
-	const textarea = editMessageTextarea.value[0]
-	if (!textarea) return
-
-	//reset la hauteur
-	textarea.style.height = 'auto'
-
-	const style = window.getComputedStyle(textarea)
-	const lineHeight = parseFloat(style.lineHeight)
-	const padding = parseFloat(style.paddingTop) + parseFloat(style.paddingBottom)
-
-	const maxLines = 5
-	const maxHeight = lineHeight * maxLines + padding
-
-	textarea.style.height = Math.min(textarea.scrollHeight, maxHeight) + 'px'
-	textarea.style.height = textarea.scrollHeight + 'px'
-	textarea.style.width = '50%'
-})
 </script>
 <template>
 	<div class="home">
@@ -489,57 +406,14 @@ watch(messagetoEdit, async () => {
 			</div>
 		</div>
 		<div class="current-conv">
-			<div class="messages" @scroll="onScroll" ref="messagesContainer">
-				<div
-					v-for="(message, index) in messages"
-					class="message"
-					:class="{ 'self-message': message.user_id == userStore.user.user_id }"
-				>
-					<div class="message-sender" v-if="shouldShowUsername(index)">
-						{{ message.User.name }}
-					</div>
-					<div
-						class="message-text"
-						v-if="!editInputShown || editInputShown != message.message_id"
-						@click="showMessageMenu(message.message_id)"
-						v-html="parseMarkdown(message.text)"
-					></div>
-					<form
-						class="edit-message"
-						v-else
-						@submit.prevent="editMessage(message.message_id)"
-					>
-						<textarea
-							placeholder="Modifier le message"
-							v-model="messagetoEdit"
-							ref="editMessageTextarea"
-						></textarea>
-						<button @click="closeEditMessage" type="button">
-							<img src="../assets/close.png" />
-						</button>
-						<button type="submit"><img src="../assets/check.png" /></button>
-					</form>
-					<div
-						class="message-menu"
-						v-if="
-							messageMenuShown &&
-							messageMenuShown == message.message_id &&
-							(message.user_id == userStore.user.user_id || //afficher le menu seulement sur nos message ou sur tout les message si on est l'admin de la conversation actuelle
-								userStore.user.user_id == shownConvAdminId)
-						"
-					>
-						<button @click="showEditMessage(message)">
-							<img src="../assets/edit.png" />
-						</button>
-						<button @click="deleteMessage(message.message_id)">
-							<img src="../assets/delete.png" />
-						</button>
-						<button @click.stop="closeMessageMenu">
-							<img src="../assets/close.png" />
-						</button>
-					</div>
-				</div>
-			</div>
+			<Messages
+				@scroll="onScroll"
+				ref="messagesContainer"
+				:messages="messages"
+				:shown-conv-id="shownConvId"
+				:shown-conv-admin-id="shownConvAdminId"
+				class="messages"
+			></Messages>
 			<button v-if="!isUserAtBottom" class="newMessageButton" @click="scrollToBottom">
 				Nouveaux messages
 			</button>
@@ -680,82 +554,6 @@ watch(messagetoEdit, async () => {
 	flex-direction: column;
 	margin-left: 15px;
 	/* max-height: 100%; */
-}
-
-.messages {
-	flex: 1;
-	overflow-y: auto;
-}
-
-.message {
-	display: flex;
-	flex-direction: column;
-	gap: 4px;
-	margin-top: 4px;
-}
-
-.message-sender {
-	font-size: 14px;
-	font-weight: bold;
-	margin-top: 5px;
-}
-
-.message-text {
-	background: blue;
-	padding: 0px 10px;
-	width: fit-content;
-	max-width: 50%;
-
-	word-wrap: break-word;
-	overflow-wrap: break-word;
-	overflow-wrap: anywhere;
-	text-align: left;
-	white-space: normal;
-}
-
-.message-text :link {
-	color: #00ffff;
-}
-
-/* deep pour que le css s'applique*/
-:deep(.message-text img) {
-	max-width: 100%;
-}
-
-.message-menu button {
-	width: 40px;
-	margin: 5px;
-}
-
-.message-menu button img {
-	width: 100%;
-}
-
-.edit-message textarea {
-	padding: 10px;
-	margin: 5px;
-	line-height: 20px;
-	resize: none;
-}
-
-.edit-message button {
-	width: 40px;
-	margin: 5px;
-}
-
-.edit-message button img {
-	width: 100%;
-}
-
-.self-message {
-	align-self: flex-end;
-	text-align: right;
-	margin-right: 15px;
-}
-
-.self-message .message-text {
-	background: darkblue;
-	margin-left: auto;
 }
 
 .createConv {
