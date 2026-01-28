1ad542e4c122ba7981b12f4fa3c815be64df6781
Bastien <po77qxd@eduvaud.ch>
2026-01-28 15:28:18 +0100

    refactor(frontend): transformer la bar de message en composant

--- frontend/src/views/UserHomeView.vue
+++ frontend/src/views/UserHomeView.vue
@@ -2,17 +2,14 @@
 import { ref, onMounted, nextTick, watch, computed } from 'vue'
 import conversationService from '@/services/conversationService'
 import userService from '@/services/userService'
-import messageService from '@/services/messageService'
+
 import userConversationService from '@/services/userConversationService'
-import uploadService from '@/services/uploadService'
 import { useUserStore } from '@/stores/user'
 import DOMPurify from 'dompurify'
 import { marked } from 'marked'
 
-import axios from 'axios'
-
 import { socket } from '@/main'
-import gifService from '@/services/gifService'
+import MessageBar from '@/components/MessageBar.vue'
 
 const userStore = useUserStore()
 
@@ -22,7 +19,6 @@ const shownConvId = ref(null)
 const shownConvAdminId = ref(null)
 const showCreateConv = ref(false)
 const convName = ref('')
-const messageToSend = ref('')
 const messageMenuShown = ref(null) //id of the message where the menu is shown
 const editInputShown = ref(null) //id of the message being modified
 const messagetoEdit = ref(null)
@@ -39,21 +35,7 @@ const messagesContainer = ref(null)
 const isUserAtBottom = ref(true)
 const SCROLL_THRESHOLD = 50 //px
 
-const sendMessageTextarea = ref(null)
 const editMessageTextarea = ref(null)
-const typingText = computed(() => {
-	const typingUsers = userList.value?.filter((user) => user.isTyping).map((user) => user.name)
-	if (!typingUsers) return
-
-	const count = typingUsers.length
-	if (count == 0) return ''
-	if (count == 1) return `${typingUsers[0]} est en train d'écrire`
-	if (count <= 3) return `${typingUsers.join(', ')} sont en train d'écrire`
-	return "Plusieurs utilisateurs sont en train d'écrire"
-})
-const showGifPicker = ref(false)
-const gifQuery = ref('')
-const gifs = ref([])
 
 //sockets:
 socket.on('new_message', async (message) => {
@@ -240,20 +222,6 @@ async function createConv() {
 		})
 }
 
-async function sendMessage() {
-	if (!messageToSend.value.trim()) return
-
-	conversationService
-		.sendMessage(shownConvId.value, messageToSend.value)
-		.then((response) => {
-			messageToSend.value = ''
-			//console.log(response.data.data)
-		})
-		.catch((error) => {
-			console.log(error)
-		})
-}
-
 function showMessageMenu(id) {
 	messageMenuShown.value = id
 }
@@ -425,25 +393,6 @@ function parseMarkdown(text) {
 	return DOMPurify.sanitize(marked.parse(text))
 }
 
-watch(messageToSend, async () => {
-	await nextTick()
-
-	const textarea = sendMessageTextarea.value
-	if (!textarea) return
-
-	// reset la hauteur
-	textarea.style.height = 'auto'
-
-	const style = window.getComputedStyle(textarea)
-	const lineHeight = parseFloat(style.lineHeight)
-	const padding = parseFloat(style.paddingTop) + parseFloat(style.paddingBottom)
-
-	const maxLines = 5
-	const maxHeight = lineHeight * maxLines + padding
-
-	textarea.style.height = Math.min(textarea.scrollHeight, maxHeight) + 'px'
-})
-
 watch(messagetoEdit, async () => {
 	await nextTick()
 
@@ -464,84 +413,6 @@ watch(messagetoEdit, async () => {
 	textarea.style.height = textarea.scrollHeight + 'px'
 	textarea.style.width = '50%'
 })
-
-async function uploadImage(e) {
-	const file = e.target.files[0]
-	if (!file) return
-
-	const formData = new FormData()
-	formData.append('image', file)
-
-	const res = await uploadService.uploadImage(formData)
-
-	messageToSend.value += `\n![image](${res.data.url})`
-}
-
-async function handleImageDrop(e) {
-	const file = e.dataTransfer.files[0]
-	if (!file || !file.type.startsWith('image/')) return
-
-	const formData = new FormData()
-	formData.append('image', file)
-
-	const res = await uploadService.uploadImage(formData)
-
-	messageToSend.value += `\n![image](${res.data.url})`
-}
-
-function typing() {
-	socket.emit('typing', shownConvId.value)
-}
-
-function toggleGifPicker() {
-	showGifPicker.value = !showGifPicker.value
-	if (showGifPicker.value) searchGif()
-}
-
-async function searchGif() {
-	//si la recherche est vide, on affiche les gifs trending
-	if (gifQuery.value == '') {
-		getTrendingGifs()
-		return
-	}
-	const res = await gifService.searchGif(gifQuery.value)
-	gifs.value = res.data.data
-}
-
-async function getTrendingGifs() {
-	const res = await gifService.getTrendingGifs()
-	gifs.value = res.data.data
-}
-function sendGif(gifTitle, gifUrl) {
-	messageToSend.value += `![${gifTitle}](${gifUrl})`
-}
-
-const leftColumn = ref([])
-const rightColumn = ref([])
-
-watch(
-	() => gifs.value,
-	(newGifs) => {
-		leftColumn.value = []
-		rightColumn.value = []
-
-		let leftHeight = 0
-		let rightHeight = 0
-
-		newGifs.forEach((gif) => {
-			const h = gif.images.fixed_height.height
-
-			if (leftHeight <= rightHeight) {
-				leftColumn.value.push(gif)
-				leftHeight += Number(h)
-			} else {
-				rightColumn.value.push(gif)
-				rightHeight += Number(h)
-			}
-		})
-	},
-	{ immediate: true },
-)
 </script>
 <template>
 	<div class="home">
@@ -672,66 +543,11 @@ watch(
 			<button v-if="!isUserAtBottom" class="newMessageButton" @click="scrollToBottom">
 				Nouveaux messages
 			</button>
-			<div class="message-bar">
-				<div class="typingUsers" v-if="typingText">{{ typingText }}</div>
-				<div v-if="showGifPicker" class="gifPicker">
-					<input
-						type="text"
-						@input="searchGif"
-						placeholder="Rechercher un gif"
-						v-model="gifQuery"
-					/>
-					<div class="gifGrid">
-						<div class="gifCol">
-							<img
-								v-for="gif in leftColumn"
-								:key="gif.id"
-								:src="gif.images.fixed_height.url"
-								@click="sendGif(gif.title, gif.images.original.url)"
-								class="gifGridImg"
-							/>
-						</div>
-
-						<div class="gifCol">
-							<img
-								v-for="gif in rightColumn"
-								:key="gif.id"
-								:src="gif.images.fixed_height.url"
-								@click="sendGif(gif.title, gif.images.original.url)"
-								class="gifGridImg"
-							/>
-						</div>
-					</div>
-				</div>
-				<form @submit.prevent="sendMessage" class="sendMessageForm">
-					<textarea
-						placeholder="Envoyer un message"
-						v-model="messageToSend"
-						@keypress.enter.exact="sendMessage"
-						rows="1"
-						ref="sendMessageTextarea"
-						@dragover.prevent=""
-						@drop.prevent="handleImageDrop"
-						maxlength="4096"
-						@input="typing"
-					></textarea>
-					<input
-						type="file"
-						ref="fileInput"
-						accept="image/*"
-						hidden
-						@change="uploadImage"
-					/>
-
-					<button type="submit" class="sendButton">
-						<img src="../assets/send.png" />
-					</button>
-					<button @click="$refs.fileInput.click()" class="uploadImageButton">
-						<img src="../assets/image.png" />
-					</button>
-					<button @click="toggleGifPicker" type="button">GIF</button>
-				</form>
-			</div>
+			<MessageBar
+				class="message-bar"
+				:user-list="userList"
+				:shown-conv-id="shownConvId"
+			></MessageBar>
 		</div>
 		<div
 			class="userList"
@@ -942,57 +758,6 @@ watch(
 	margin-left: auto;
 }
 
-.typingUsers {
-	display: flex;
-}
-
-.sendMessageForm {
-	display: flex;
-}
-
-.message-bar {
-	width: 100%;
-	margin-top: 15px;
-	display: flex;
-	flex-direction: column;
-}
-
-.message-bar textarea {
-	height: 50px;
-	flex: 1;
-	padding: 15px;
-	padding-right: 35px;
-	width: 90%;
-	word-wrap: break-word;
-	overflow-wrap: break-word;
-	text-align: left;
-	line-height: 20px;
-	resize: none;
-	box-sizing: border-box;
-}
-
-.message-bar .sendButton {
-	cursor: pointer;
-	width: 40px;
-	margin: 5px;
-	border: none;
-	margin-left: -40px;
-	background-color: white;
-}
-
-.message-bar .uploadImageButton {
-	width: 40px;
-	margin-right: 5px;
-}
-
-.message-bar .uploadImageButton img {
-	width: 100%;
-}
-
-.sendButton img {
-	width: 100%;
-}
-
 .createConv {
 	width: 100%;
 	text-align: center;
@@ -1052,54 +817,5 @@ watch(
 
 .userListShownClass button {
 	margin-bottom: 10px;
-}
-
-.gifPicker {
-	align-items: end;
-	display: flex;
-	flex-direction: column;
-	margin: 5px;
-	width: 25%;
-	align-self: end;
-	background-color: gray;
-	z-index: 999;
-	margin-top: -475px;
-}
-
-.gifPicker input {
-	padding: 10px;
-	margin-bottom: 5px;
-	margin-top: 5px;
-	width: 85%;
-	align-self: center;
-}
-
-.gifPicker img:hover {
-	cursor: pointer;
-	/* opacity: 75%; */
-	transform: scale(1.05);
-	transition: transform 0.15s ease;
-}
-
-.gifGrid {
-	height: 400px;
-	width: 300px;
-	display: flex;
-	gap: 10px;
-	overflow-y: auto;
-	padding: 10px;
-}
-
-.gifCol {
-	flex: 1;
-	display: flex;
-	flex-direction: column;
-	gap: 10px;
-}
-
-.gifGridImg {
-	width: 100%;
-	border-radius: 10px;
-	cursor: pointer;
 }
 </style>
